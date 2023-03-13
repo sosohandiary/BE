68675f0e5c2cec8d1c39345a243f81cd0cd158f8
1juuun <potenter11@gmail.com>
2023-03-13 23:19:23 +0900

    Feat: 카카오 로그인 기능  WebSecurityConfig 코드 추가 #3

--- src/main/java/com/hanghae/sosohandiary/config/WebSecurityConfig.java
+++ src/main/java/com/hanghae/sosohandiary/config/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package com.hanghae.sosohandiary.config;
 
+import com.hanghae.sosohandiary.auth.JwtAuthFilter;
 import com.hanghae.sosohandiary.auth.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,6 +27,37 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.cors();
+        httpSecurity.csrf().disable();
+        httpSecurity.formLogin().disable();
+
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//        httpSecurity.exceptionHandling()
+//                .authenticationEntryPoint(authenticationEntryPoint)
+//                .and();
+
+        httpSecurity.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+//                .and().exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        httpSecurity.formLogin().permitAll();
+
+        return httpSecurity.build();
     }
 
 }
