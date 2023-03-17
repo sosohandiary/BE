857eab62376866702b43ebe652687d0f3501860c
“eastjin” <afaset4444@naver.com>
2023-03-17 22:23:07 +0900

    Feat: 내가 등록한 친구 조회

--- src/main/java/com/hanghae/sosohandiary/domain/myfriendsList/repository/MyFriendsListRepository.java
+++ src/main/java/com/hanghae/sosohandiary/domain/myfriendsList/repository/MyFriendsListRepository.java
@@ -3,5 +3,8 @@ package com.hanghae.sosohandiary.domain.myfriendsList.repository;
 import com.hanghae.sosohandiary.domain.myfriendsList.entity.MyFriendsList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MyFriendsListRepository extends JpaRepository<MyFriendsList, Long> {
+    List<MyFriendsList> findAllById(Long id);
 }
