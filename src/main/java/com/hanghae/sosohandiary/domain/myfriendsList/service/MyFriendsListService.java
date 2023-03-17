843727f172efb1fe96320907996a886d682be769
“eastjin” <afaset4444@naver.com>
2023-03-17 20:52:28 +0900

    Feat: 친구등록 수락 API 구현 #27
    친구등록 수락 API 구현 #27

--- src/main/java/com/hanghae/sosohandiary/domain/myfriendsList/service/MyFriendsListService.java
+++ src/main/java/com/hanghae/sosohandiary/domain/myfriendsList/service/MyFriendsListService.java
@@ -7,6 +7,8 @@ import com.hanghae.sosohandiary.domain.myfriendsList.entity.FriendRequest;
 import com.hanghae.sosohandiary.domain.myfriendsList.entity.MyFriendsList;
 import com.hanghae.sosohandiary.domain.myfriendsList.repository.FriendRequestRepository;
 import com.hanghae.sosohandiary.domain.myfriendsList.repository.MyFriendsListRepository;
+import com.hanghae.sosohandiary.exception.ApiException;
+import com.hanghae.sosohandiary.exception.ErrorHandling;
 import com.hanghae.sosohandiary.utils.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -65,25 +67,27 @@ public class MyFriendsListService {
 
     @Transactional
     public MessageDto createFriendAccept(Long id, Member member) {
-        Optional<Member> userId = memberRepository.findById(member.getId());
-        Optional<Member> friendId = memberRepository.findById(id);
+        Optional<FriendRequest> addId = friendRequestRepository.findById(id);
 
+        if (!addId.get().getFriend().getId().equals(member.getId())) {
+            throw new ApiException(ErrorHandling.NOT_MATCH_AUTHORIZATION);
+        }
 
         MyFriendsList myFriendsList = MyFriendsList.builder()
-                .member(userId.get())
-                .friend(friendId.get())
+                .member(addId.get().getMember())
+                .friend(addId.get().getFriend())
                 .build();
 
 
         MyFriendsList myFriendsList2 = MyFriendsList.builder()
-                .member(friendId.get())
-                .friend(userId.get())
+                .member(addId.get().getFriend())
+                .friend(addId.get().getMember())
                 .build();
 
         myFriendsListRepository.save(myFriendsList);
         myFriendsListRepository.save(myFriendsList2);
-        //todo : delete friendRequest 삭제로직
-        return new MessageDto("친구추가 성공", HttpStatus.CREATED);
+        friendRequestRepository.deleteById(id);
+        return new MessageDto("친구추가 성공", HttpStatus.ACCEPTED);
     }
 
 
