33a0b8177071e181466dd20ae192acbec5232283
1juuun <potenter11@gmail.com>
2023-04-03 20:37:18 +0900

    Feat: 프로필 사진 추가하기 기능 #114
    프로필 이미지가 친구추가 요청 목록, 친구 목록 조회에 profileImageUrl  response에 데이터 넣어 주기

--- src/main/java/com/hanghae/sosohandiary/domain/friend/dto/FriendResponseDto.java
+++ src/main/java/com/hanghae/sosohandiary/domain/friend/dto/FriendResponseDto.java
@@ -9,19 +9,22 @@ public class FriendResponseDto {
     private Long friendListId;
     private String friendNickName;
     private String nickname;
+    private String profileImageUrl;
 
     @Builder
-    private FriendResponseDto(Long id, String friendNickName, String nickname) {
+    private FriendResponseDto(Long id, String friendNickName, String nickname, String profileImageUrl) {
         this.friendListId = id;
         this.nickname = nickname;
         this.friendNickName = friendNickName;
+        this.profileImageUrl = profileImageUrl;
     }
 
-    public static FriendResponseDto of(Long id, String friendNickName, String nickname) {
+    public static FriendResponseDto of(Long id, String friendNickName, String nickname, String profileImageUrl) {
         return FriendResponseDto.builder()
                 .id(id)
                 .friendNickName(friendNickName)
                 .nickname(nickname)
+                .profileImageUrl(profileImageUrl)
                 .build();
     }
 
